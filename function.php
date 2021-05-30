53b5b409bbc42921be9c4b2d0dc153b5dfbad945
Gianluigi Ceglie <gianluig.ceglie@gmail.com>
2021-05-30 17:09:23 +0200

    Aggiunta funzione sottrazione

--- function.php
+++ function.php
@@ -1,8 +1,13 @@
 <? 
 
 /* addizione */
-Function addizione (n1,n2){
- return n1 +n2;
+function addizione (n1,n2){
+ return n1 + n2;
+}
+
+/* sottrazione */
+function sottrazione (n1,n2){
+ return n1 - n2;
 }
 
 ?>
